0baeff66998a880126b9513d0e2f6b8112669c43
syaki <syaki@KILA>
2022-07-21 01:51:54 +0800

    new

---
+++ Camp Booking System/src/main/java/CampJavabean/Staff.java
@@ -0,0 +1,33 @@
+package CampJavabean;
+
+public class Staff {
+    String staff_name;
+    String staff_email;
+    String staff_password;
+    String staff_phonenum;
+    
+    public String getStaff_name() {
+        return staff_name;
+    }
+    public void setStaff_name(String staff_name) {
+        this.staff_name = staff_name;
+    }
+    public String getStaff_email() {
+        return staff_email;
+    }
+    public void setStaff_email(String staff_email) {
+        this.staff_email = staff_email;
+    }
+    public String getStaff_password() {
+        return staff_password;
+    }
+    public void setStaff_password(String staff_password) {
+        this.staff_password = staff_password;
+    }
+    public String getStaff_phonenum() {
+        return staff_phonenum;
+    }
+    public void setStaff_phonenum(String staff_phonenum) {
+        this.staff_phonenum = staff_phonenum;
+    }
+}
