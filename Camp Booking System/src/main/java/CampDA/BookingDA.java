51b302c1cd054a299ed5139e4054491a5d8993da
Akimni <muhammadhakimhafidz@gmail.com>
2022-07-21 15:35:35 +0800

    commit

--- Camp Booking System/src/main/java/CampDA/BookingDA.java
+++ Camp Booking System/src/main/java/CampDA/BookingDA.java
@@ -12,7 +12,7 @@ public class BookingDA {
 	static PreparedStatement ps = null;
 	static Statement stmt = null;
 	static ResultSet rs = null;
-	
+	  
 	static String dbURL = "jdbc:postgresql://ec2-34-239-241-121.compute-1.amazonaws.com/dfogu31uq8h3mb";
 	static String user = "kiljgbsypcqcne";
 	static String pass = "da31ec8fde345a6ed0bc0724f57aa469269b1a98b68fa054da0d20f7b7894763";
